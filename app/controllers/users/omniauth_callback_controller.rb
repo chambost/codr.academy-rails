f0bd46825c1f2735b38776146f3d82e480db62db
Adam Chambost <chambost@users.noreply.github.com>
2020-05-04 18:26:25 +0800

    Callback fixes

--- app/controllers/users/omniauth_callback_controller.rb
+++ app/controllers/users/omniauth_callback_controller.rb
@@ -7,13 +7,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
         set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
       else
-        @user.email = request.env["omniauth.auth"]["extra"]["all_emails"][0]["email"]
-        @user.name = request.env["omniauth.auth"]["info"]["name"]
-        @user.username = request.env["omniauth.auth"]["info"]["nickname"]
-        @user.github = "https://github.com/#{@user.username}"
-        @user.save
-        sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: "GitHub") if is_navigational_format?
+        # @user.email = request.env["omniauth.auth"]["extra"]["all_emails"][0]["email"]
+        # user.password = Devise.friendly_token[0, 20]
+        # @user.name = request.env["omniauth.auth"]["info"]["name"]
+        # @user.username = request.env["omniauth.auth"]["info"]["nickname"]
+        # @user.github = "https://github.com/#{@user.username}"
+        # @user.save
+        # sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+        # set_flash_message(:notice, :success, kind: "GitHub") if is_navigational_format?
+        redirect_to new_user_registration_url
       end
     end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   resources :posts
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users
   root 'home_page#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
