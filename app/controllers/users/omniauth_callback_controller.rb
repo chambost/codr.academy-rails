5d0cedb6e0f90cc7d9c95cb24d7d40ae2c221d29
Adam Chambost <chambost@users.noreply.github.com>
2020-05-04 17:55:56 +0800

    github instead of facebook

--- app/controllers/users/omniauth_callback_controller.rb
+++ app/controllers/users/omniauth_callback_controller.rb
@@ -1,11 +1,11 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def facebook
+    def github
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env["omniauth.auth"])
   
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+        set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
       else
         @user.email = request.env["omniauth.auth"]["extra"]["all_emails"][0]["email"]
         @user.name = request.env["omniauth.auth"]["info"]["name"]
