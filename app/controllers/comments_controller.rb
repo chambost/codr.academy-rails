545208a96cb90f20c89d3a2fc0e7e22e0c70100f
Adam Chambost <chambost@users.noreply.github.com>
2020-05-07 14:45:33 +0800

    Redirect to root_path after comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,7 +33,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
