6fd442205e1628504faa32551708eb65d5dd0c46
bcehmu <bcehmu@hotmail.com>
2020-05-08 09:35:29 +1000

    Update verbal_mark.rb model and begin styling

--- app/models/verbal_mark.rb
+++ app/models/verbal_mark.rb
@@ -2,4 +2,6 @@ class VerbalMark < ApplicationRecord
   belongs_to :user
   belongs_to :post
   belongs_to :comment
+  # /^([a-z]{1,30}){1}(_{1}[a-z]{1,30}){,1}(_{1}[a-z]{1,30}){,1}\Z/
+  validates :verbal_mark, format: { with: /\A([a-z]{1,30}){1}(_{1}[a-z]{1,30}){,1}(_{1}[a-z]{1,30}){,1}\Z/, message: "Each verbal_mark should be in lowercase, snake_case, 3 words at most, 30 letters at most for each word."}
 end
